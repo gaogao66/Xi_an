e395d94f62382e87be750457df1d631f03690b7b
gaogao66 <3163846872@qq.com>
2019-04-29 10:31:59 +0800

    解决音乐问题

--- Xi_an/js/music.js
+++ Xi_an/js/music.js
@@ -1,6 +1,11 @@
+$(document).ready(function(){
+	var time = localStorage.getItem('curTime');
+    // console.log('s',time);
+    just_play(time);
+});
 function play_music(){
 	if ($('#mc_play').hasClass('on')){
-		$('#mc_play audio').get(0).pause();
+        $('#mc_play audio').get(0).pause();
 		$('#mc_play').attr('class','stop');
 	}else{
 		$('#mc_play audio').get(0).play();
@@ -9,8 +14,16 @@ function play_music(){
 	$('#music_play_filter').hide();
 	event.stopPropagation(); //阻止冒泡
 }
-function just_play(id){
-	$('#mc_play audio').get(0).play();
+function just_play(time){
+	var audio = $('#mc_play audio');
+	if(time) audio.get(0).currentTime = time;
+	audio.get(0).play();
 	$('#mc_play').attr('class','on');
-	event.stopPropagation(); //阻止冒泡
+	 // event.stopPropagation(); //阻止冒泡
 }
+window.onunload=function(){
+	var curTime = document.querySelector('#mc_play audio').currentTime;
+	console.log('e',curTime);
+
+    localStorage.setItem('curTime',curTime);
+};
